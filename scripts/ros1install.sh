a0f058ca3ee71a11f46d04b950d2c447a4b092d6
Andrew Vataev <avataev@yandex.ru>
2025-10-29 00:45:54 +0300

    внесены правки ros1install

--- scripts/ros1install.sh
+++ scripts/ros1install.sh
@@ -8,20 +8,20 @@ fi
 
 #Выполним установку и настройку системной локали
 echo "Добавление в sources.list-директорию файл с сервером, где хранятся пакеты для ROS1..."
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 #Выполним установку и настройку curl
 echo "Установка curl..."
-apt install curl
+sudo apt install curl 
+
 
 #Добавление репозитория ROS1
 echo "Добавление репозитория ROS1..."
-curl -s 
-https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 #Установка ROS1
 echo "Установка ROS1 full версии..."
-#sudo apt update
+sudo apt update
 sudo apt install ros-noetic-desktop-full
 
 #Настройка окружения
