ff0ca55291755f0a77872c75a2c77e3df38b057d
Andrew Vataev <avataev@yandex.ru>
2025-10-31 19:16:10 +0300

    исправлены опечатки в ros1install

--- scripts/ros1install.sh
+++ scripts/ros1install.sh
@@ -72,7 +72,8 @@ done
 
 # Добавление репозитория ROS1
 log_msg "Добавление репозитория ROS1..."
-log_msg "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+#log_msg "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list
 
 if ! curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -; then
     log_msg "Ошибка: не удалось добавить GPG‑ключ ROS"
